843c655bbc610013fe303bba645c67bbd4919120
Tonbra Benjamin Yerekimea <yerekimeatonbaa@gmail.com>
2025-12-04 13:11:01 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    allowedDevOrigins: [
+        "http://localhost:9003",
+        "https://*.cloudworkstations.dev",
+        "https://*.firebase.studio"
+    ],
+  },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
+};
+
+export default nextConfig;
